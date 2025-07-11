127562f17b55366211a69f4e1eb3f064f80bb7c1
Jacques <jacquesvanluyck@gmail.com>
2025-07-11 13:11:14 +0200

    build new task component and fixed some bugs in media queries ,fixes #10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FilterControlsComponent } from "./filter-controls/filter-controls.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FilterControlsComponent],
+  imports: [HeaderComponent, FilterControlsComponent, NewTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
